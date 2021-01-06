f7052c559d01d242c38428074d783957187abfaf
Marshall Belles <marshall.belles@yahoo.com>
2021-01-06 16:24:56 -0500

    changes to fix bugs in server-deploy.sh

--- server-deploy.sh
+++ server-deploy.sh
@@ -6,8 +6,8 @@ for proc in $NODE; do kill $proc; done;
 
 # Prepare aesir-wolf-pack
 cd aesir-wolf-pack;
-npm -i;
-tsc;
+npm i;
+tsc .;
 mkdir /opt/bots/production/aesir-wolf-pack;
 cp build/app.js /opt/bots/production/aesir-wolf-pack/awp.js;
 cp package.json /opt/bots/production/aesir-wolf-pack/package.json;
@@ -16,17 +16,17 @@ cd ..;
 
 # Prepare industry-bot
 cd industry-bot;
-npm -i;
-tsc;
+npm i;
+tsc .;
 mkdir /opt/bots/production/industry-bot;
 cp build/app.js /opt/bots/production/industry-bot/inb.js;
 cp package.json /opt/bots/production/industry-bot/package.json;
 cp package-lock.json /opt/bots/production/industry-bot/package-lock.json;
 
 cd /opt/bots/production/aesir-wolf-pack;
-npm -i;
+npm i;
 nohup node awp.js &
 
 cd ../industry-bot;
-npm -i;
+npm i;
 nohup node inb.js &
