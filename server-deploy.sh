755afe9063a32b3ecd8d1491abb39f8f71ebfa7f
Marshall Belles <marshall.belles@yahoo.com>
2021-01-06 16:47:45 -0500

    updated server-deploy.sh

--- server-deploy.sh
+++ server-deploy.sh
@@ -7,9 +7,12 @@ for proc in $NODE; do kill $proc; done;
 # Prepare aesir-wolf-pack
 cd aesir-wolf-pack;
 npm i;
+rm -rf build;
 tsc .;
 rm -rf /opt/bots/production/aesir-wolf-pack;
 mkdir /opt/bots/production/aesir-wolf-pack;
+mkdir build;
+mv ./app.js build;
 cp build/app.js /opt/bots/production/aesir-wolf-pack/awp.js;
 cp package.json /opt/bots/production/aesir-wolf-pack/package.json;
 cp package-lock.json /opt/bots/production/aesir-wolf-pack/package-lock.json;
@@ -18,9 +21,12 @@ cd ..;
 # Prepare industry-bot
 cd industry-bot;
 npm i;
+rm -rf build;
 tsc .;
 rm -rf /opt/bots/production/industry-bot;
 mkdir /opt/bots/production/industry-bot;
+mkdir build;
+mv ./app.js build;
 cp build/app.js /opt/bots/production/industry-bot/inb.js;
 cp package.json /opt/bots/production/industry-bot/package.json;
 cp package-lock.json /opt/bots/production/industry-bot/package-lock.json;
